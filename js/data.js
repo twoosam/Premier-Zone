b0d4cca8df7f69b9589fb3ca733c682dd7f28da6
Tim Woo-Sam <twoosam10@gmail.com>
2023-08-15 21:07:32 +0000

    Created the HTML for the different features tab and current purple tab.

--- js/data.js
+++ js/data.js
@@ -1,14 +1,1 @@
 /* exported data */
-// function getFootballData() {
-//   const xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'https://v3.football.api-sports.io/standings?league=39&season=2023');
-//   xhr.setRequestHeader('x-rapidapi-host', 'v3.football.api-sports.io');
-//   xhr.setRequestHeader('x-apisports-key', '8ad7209e9e0a016c96f4e199bed14b5c');
-//   xhr.responseType = 'json';
-//   xhr.addEventListener('load', function () {
-//     console.log(xhr.status);
-//     console.log(xhr.response);
-//   });
-//   xhr.send();
-// }
-// getFootballData();
