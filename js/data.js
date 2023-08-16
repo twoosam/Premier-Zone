30f0b8f3f8159a672ed9669f33a30fc08cd01b53
Tim Woo-Sam <twoosam10@gmail.com>
2023-08-16 02:57:13 +0000

    Queryed the DOM for '#standings-table' element, looped through the response of the object, and appended new tr and td elements for each team.

--- js/data.js
+++ js/data.js
@@ -1,4 +1,5 @@
 /* exported data */
+const $teamStandings = document.querySelector('#appendTeams');
 function getPremierLeagueData() {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://v3.football.api-sports.io/standings?league=39&season=2023');
@@ -8,6 +9,26 @@ function getPremierLeagueData() {
   xhr.addEventListener('load', function () {
     // console.log(xhr.status);
     // console.log(xhr.response);
+    for (let i = 0; i < xhr.response.response[0].league.standings[0].length; i++) {
+      const $newTr = document.createElement('tr');
+      const $newTdTeam = document.createElement('td');
+      const $newTdWin = document.createElement('td');
+      const $newTdDraw = document.createElement('td');
+      const $newTdLoss = document.createElement('td');
+      const $newTdPoints = document.createElement('td');
+      $teamStandings.appendChild($newTr);
+      $newTdTeam.textContent = xhr.response.response[0].league.standings[0][i].rank + ' ' + xhr.response.response[0].league.standings[0][i].team.name;
+      $newTr.appendChild($newTdTeam);
+      $newTdWin.textContent = xhr.response.response[0].league.standings[0][i].all.win;
+      $newTr.appendChild($newTdWin);
+      $newTdDraw.textContent = xhr.response.response[0].league.standings[0][i].all.draw;
+      $newTr.appendChild($newTdDraw);
+      $newTdLoss.textContent = xhr.response.response[0].league.standings[0][i].all.lose;
+      $newTr.appendChild($newTdLoss);
+      $newTdPoints.textContent = xhr.response.response[0].league.standings[0][i].points;
+      $newTr.appendChild($newTdPoints);
+    }
   });
+  xhr.send();
 }
 getPremierLeagueData();
