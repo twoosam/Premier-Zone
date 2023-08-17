09e47e38203cdaa2036169ea46235c8ce992ecdd
Tim Woo-Sam <twoosam10@gmail.com>
2023-08-17 18:08:15 +0000

    Created function outside of request and called it back in the for loop.

--- js/data.js
+++ js/data.js
@@ -9,40 +9,44 @@ function getPremierLeagueData() {
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     for (let i = 0; i < xhr.response.response[0].league.standings[0].length; i++) {
-      const $newTr = document.createElement('tr');
-      const $newTdBlank = document.createElement('td');
-      const $newTdRank = document.createElement('ins');
-      const $newTdLogo = document.createElement('img');
-      const $newTdTeam = document.createElement('ins');
-      const $newTdWin = document.createElement('td');
-      const $newTdDraw = document.createElement('td');
-      const $newTdLoss = document.createElement('td');
-      const $newTdPoints = document.createElement('td');
-      $teamStandings.appendChild($newTr);
-      $newTdBlank.setAttribute('class', 'rank-logo-name');
-      $newTr.appendChild($newTdBlank);
-      $newTdRank.textContent = xhr.response.response[0].league.standings[0][i].rank;
-      $newTdRank.setAttribute('class', 'rank');
-      $newTdBlank.appendChild($newTdRank);
-      $newTdLogo.setAttribute('src', xhr.response.response[0].league.standings[0][i].team.logo);
-      $newTdLogo.setAttribute('class', 'logo-img');
-      $newTdBlank.appendChild($newTdLogo);
-      $newTdTeam.textContent = xhr.response.response[0].league.standings[0][i].team.name;
-      $newTdBlank.appendChild($newTdTeam);
-      $newTdWin.textContent = xhr.response.response[0].league.standings[0][i].all.win;
-      $newTdWin.setAttribute('class', 'center-standing');
-      $newTr.appendChild($newTdWin);
-      $newTdDraw.textContent = xhr.response.response[0].league.standings[0][i].all.draw;
-      $newTdDraw.setAttribute('class', 'center-standing');
-      $newTr.appendChild($newTdDraw);
-      $newTdLoss.textContent = xhr.response.response[0].league.standings[0][i].all.lose;
-      $newTdLoss.setAttribute('class', 'center-standing');
-      $newTr.appendChild($newTdLoss);
-      $newTdPoints.textContent = xhr.response.response[0].league.standings[0][i].points;
-      $newTdPoints.setAttribute('class', 'center-standing');
-      $newTr.appendChild($newTdPoints);
+      standingsTable(xhr.response.response[0].league.standings[0][i]);
     }
   });
   xhr.send();
 }
 getPremierLeagueData();
+
+function standingsTable(xhr) {
+  const $newTr = document.createElement('tr');
+  const $newTdBlank = document.createElement('td');
+  const $newTdRank = document.createElement('ins');
+  const $newTdLogo = document.createElement('img');
+  const $newTdTeam = document.createElement('ins');
+  const $newTdWin = document.createElement('td');
+  const $newTdDraw = document.createElement('td');
+  const $newTdLoss = document.createElement('td');
+  const $newTdPoints = document.createElement('td');
+  $teamStandings.appendChild($newTr);
+  $newTdBlank.setAttribute('class', 'rank-logo-name');
+  $newTr.appendChild($newTdBlank);
+  $newTdRank.textContent = xhr.rank;
+  $newTdRank.setAttribute('class', 'rank');
+  $newTdBlank.appendChild($newTdRank);
+  $newTdLogo.setAttribute('src', xhr.team.logo);
+  $newTdLogo.setAttribute('class', 'logo-img');
+  $newTdBlank.appendChild($newTdLogo);
+  $newTdTeam.textContent = xhr.team.name;
+  $newTdBlank.appendChild($newTdTeam);
+  $newTdWin.textContent = xhr.all.win;
+  $newTdWin.setAttribute('class', 'center-standing');
+  $newTr.appendChild($newTdWin);
+  $newTdDraw.textContent = xhr.all.draw;
+  $newTdDraw.setAttribute('class', 'center-standing');
+  $newTr.appendChild($newTdDraw);
+  $newTdLoss.textContent = xhr.all.lose;
+  $newTdLoss.setAttribute('class', 'center-standing');
+  $newTr.appendChild($newTdLoss);
+  $newTdPoints.textContent = xhr.points;
+  $newTdPoints.setAttribute('class', 'center-standing');
+  $newTr.appendChild($newTdPoints);
+}
