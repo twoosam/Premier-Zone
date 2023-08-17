570e3f6099ba83fc66df5ac6eba703f43746d8be
Tim Woo-Sam <twoosam10@gmail.com>
2023-08-17 23:57:38 +0000

    Saved progress of applying anchor tags and utilizing viewSwap function.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
-const $topScorers = document.querySelector('.top-scorers');
-const $teamStandings = document.querySelector('.standings-table');
+// viewSwap function
+const $topScorers = document.querySelector('.top-scorers-view');
+const $teamStandings = document.querySelector('.standings-table-view');
 function viewSwap(string) {
   if (string === 'top-scorers') {
-    $topScorers.setAttribute('class', '.top-scorers');
+    $topScorers.setAttribute('class', 'top-scorers-view');
     $teamStandings.setAttribute('class', 'hidden');
   } else {
     $topScorers.setAttribute('class', 'hidden');
-    $teamStandings.setAttribute('class', '.standings-table');
+    $teamStandings.setAttribute('class', 'standings-table-view');
   }
 }
-viewSwap();
+
+// Anchor for when Standings is clicked
+const $standingsAnchor = document.querySelector('.standings-anchor');
+function eventHandlerStandings() {
+  viewSwap('standings-table');
+}
+$standingsAnchor.addEventListener('click', eventHandlerStandings);
+
+// Anchor for when Top Scorers is clicked
+const $topScorersAnchor = document.querySelector('.top-scorers-anchor');
+function eventHandlerTopScorers() {
+  viewSwap('top-scorers');
+}
+$topScorersAnchor.addEventListener('click', eventHandlerTopScorers);
